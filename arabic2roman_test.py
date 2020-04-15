aacc2999a59360a468923ed9d2a6ac9184900501
Allison Foster <allisonffoster@yahoo.com>
2020-04-15 14:02:06 -0400

    pytest setup complete

---
+++ arabic2roman_test.py
@@ -0,0 +1,10 @@
+# pylint: disable=unused-variable
+"""Unit tests for Arabic2Roman."""
+
+
+def describe_an_arabic2roman_program_that():
+    """A program to convert Arabic numbers to Roman numbers."""
+
+    def has_a_smoke_test():
+        """Make sure test infrastructure is correct"""
+        assert True == True
